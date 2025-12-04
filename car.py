fe40b2922d03f2a50a13e3b9b767523a660aeb35
pranavkulkarni7-del <pranavkulkarni834@gmail.com>
2025-12-04 16:44:38 +0530

    Update car.py

--- car.py
+++ car.py
@@ -1,28 +1,21 @@
 import sys
 
+if len(sys.argv) != 4:
+    print("Usage: python program.py <car_type> <fuel_in_liters> <mileage>")
+    sys.exit()
 
-def main():
-    if len(sys.argv) != 4:
-        print("Usage: python car.py <car_type> <fuel_in_litre> <mileage>")
-        sys.exit(1)
+car_type = sys.argv[1]
+fuel = float(sys.argv[2])
+mileage = float(sys.argv[3])
 
-    car_type = sys.argv[1]
-    try:
-        fuel = float(sys.argv[2])
-        mileage = float(sys.argv[3])
-    except ValueError:
-        print("Fuel and mileage must be numeric values.")
-        sys.exit(1)
-
-    if fuel <= 0:
-        print("Fuel in litre should be greater than 0")
-        sys.exit(1)
-    elif mileage <= 0:
-        print("Mileage should be greater than 0")
-        sys.exit(1)
-
+if fuel <= 0:
+    print("Fuel must be greater than 0.")
+elif mileage <= 0:
+    print("Mileage must be greater than 0.")
+else:
     total_range = fuel * mileage
-    print(f"Car Type: {car_type}, Fuel: {fuel} litre, Mileage: {mileage} km/l")
-    print(f"The total range of the {car_type} is {total_range} km.")
+    print("Car Type:", car_type)
+    print("Fuel:", fuel, "liters")
+    print("Mileage:", mileage, "km/l")
 
-
+    print("Total Travel Range:", total_range, "km")
